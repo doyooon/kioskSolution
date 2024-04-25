e9a5b8f194e243273d64fdb1040f9b67078a1ccd
aoml3245 <aoml3245@naver.com>
2024-04-25 21:10:13 +0900

    tts stt 기능 구현 및 파이썬 기반 테스트 서버 생성

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import Dictaphone from "./component/Dictaphone";
+import Speecher from "./component/Speecher";
+
+function App() {
+  return (
+    <div className="App">
+      <Dictaphone></Dictaphone>
+      <Speecher></Speecher>
+    </div>
+  );
+}
+
+export default App;
